58db27a2e699c00468799162397b351bd3018484
ITO Nobuaki <daydream.trippers@gmail.com>
2025-12-31 12:35:16 +0900

    dispsel 0.1.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dispsel.rb
+++ Formula/dispsel.rb
@@ -1,9 +1,9 @@
 class Dispsel < Formula
   desc "CLI tool for switching monitor input sources via DDC/CI"
   homepage "https://github.com/dayflower/dispsel"
-  version "0.1.3"
-  url "https://github.com/dayflower/dispsel/releases/download/v0.1.3/dispsel-0.1.3-arm64-apple-darwin.tar.gz"
-  sha256 "a0f16a438a5a5f029db6d20e638282baae9091a21f1015546b21e9a875a3ddd6"
+  version "0.1.4"
+  url "https://github.com/dayflower/dispsel/releases/download/v0.1.4/dispsel-0.1.4-arm64-apple-darwin.tar.gz"
+  sha256 "242b56b00768a25bdf5f28c68dd89fe9048f82220d756ed3badc2a45a479c66d"
   license "MIT"
 
   depends_on arch: :arm64
