c04f8706f6ff2bdd39e90b60e9437ca203411a70
dayflower <daydream.trippers@gmail.com>
2026-01-10 11:23:20 +0900

    Add marklip template

---
+++ Formula/marklip.rb
@@ -0,0 +1,15 @@
+class Dispsel < Formula
+  desc "A lightning-fast clipboard utility for seamless Markdown ⇄ HTML conversion."
+  homepage "https://github.com/dayflower/marklip"
+  version "0.1.1"
+  url "placeholder"
+  sha256 "placeholder"
+  license "MIT"
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  def install
+    bin.install "marklip"
+  end
+end
