1954a8874bbdc03add7be88854ff8640d2fe45a7
ITO Nobuaki <daydream.trippers@gmail.com>
2026-04-21 19:13:45 +0900

    brew-up: update Casks/popmark.rb for v0.4.0
    Generated by brew-up: https://github.com/dayflower/brew-up

--- Casks/popmark.rb
+++ Casks/popmark.rb
@@ -1,8 +1,8 @@
 cask "popmark" do
-  version "0.3.3"
-  sha256 "55515e23aa4ca5772ddafe696b5f9196051d870cabab0ff8e8eee98fa683110a"
+  version "0.4.0"
+  sha256 "2d2c56307086464c656d4aa2b5d6a395e9ed9bf69d64e20eb05f0929d1681f1e"
 
-  url "https://github.com/dayflower/popmark/releases/download/v0.3.3/popmark-0.3.3-macos.zip"
+  url "https://github.com/dayflower/popmark/releases/download/v0.4.0/popmark-0.4.0-macos.zip"
   name "Popmark"
   desc "Markdown scratch-pad with global hotkey"
   homepage "https://github.com/dayflower/popmark"
