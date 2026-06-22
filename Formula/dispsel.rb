dfa2ca19c6e69340a80443766ebd7f277a0f764e
ITO Nobuaki <daydream.trippers@gmail.com>
2026-06-22 10:14:49 +0900

    brew-up: update Formula/dispsel.rb for v0.1.6

--- Formula/dispsel.rb
+++ Formula/dispsel.rb
@@ -1,13 +1,13 @@
 class Dispsel < Formula
   desc "CLI tool for switching monitor input sources via DDC/CI"
   homepage "https://github.com/dayflower/dispsel"
-  version "0.1.5"
-  url "https://github.com/dayflower/dispsel/releases/download/v0.1.5/dispsel-0.1.5-arm64-apple-darwin.tar.gz"
-  sha256 "522183c65ba723341014925b98d9e0f516664a499ee3804571d804fc2736b665"
+  version "0.1.6"
+  url "https://github.com/dayflower/dispsel/releases/download/v0.1.6/dispsel-0.1.6-arm64-apple-darwin.tar.gz"
+  sha256 "dfb427d06ad3c986593dafd2ea36ef8a8fa1973da0cd107892a2798159f63a37"
   license "MIT"
 
   depends_on arch: :arm64
-  depends_on :macos
+  depends_on macos: :ventura
 
   def install
     bin.install "dispsel"
