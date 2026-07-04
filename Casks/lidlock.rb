53cc2e6f5d589d3f358af53af9caa9395513a7cc
ITO Nobuaki <daydream.trippers@gmail.com>
2026-07-04 19:55:05 +0900

    brew-up: update Casks/lidlock.rb for v0.1.3

--- Casks/lidlock.rb
+++ Casks/lidlock.rb
@@ -1,14 +1,14 @@
 cask "lidlock" do
-  version "0.1.2"
-  sha256 "9af3f4fceca01286c6d73280d8649795951f16b2d3b9e3fc200a571aac79a9ee"
+  version "0.1.3"
+  sha256 "b1cff924f1d331c37c58dd9529e1d4bfe9f81d15346a94f59c4fa38753513df4"
 
-  url "https://github.com/dayflower/lidlock/releases/download/v0.1.2/LidLock-0.1.2.zip",
+  url "https://github.com/dayflower/lidlock/releases/download/v0.1.3/LidLock-0.1.3.zip",
       verified: "github.com/dayflower/lidlock/"
   name "LidLock"
   desc "Menu-bar app that locks or sleeps a Mac in clamshell mode"
   homepage "https://github.com/dayflower/lidlock"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "LidLock.app"
 
