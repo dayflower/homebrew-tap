8e3c1868b30bdc166b227a80518770d42c0fd1ff
ITO Nobuaki <daydream.trippers@gmail.com>
2026-07-05 14:54:37 +0900

    brew-up: update Casks/marklip-launcher.rb for v0.2.0

--- Casks/marklip-launcher.rb
+++ Casks/marklip-launcher.rb
@@ -1,18 +1,14 @@
 cask "marklip-launcher" do
-  version "0.1.3"
-  sha256 "bd70a96d01960c48af95fb07edd498c146a0bea9d31aadc0d8b00cade3a13868"
+  version "0.2.0"
+  sha256 "9dc856a37a273c6b3154c1afc996838773ba4654acab2ed3afe34010ad50cfa2"
 
-  url "https://github.com/dayflower/marklip-launcher/releases/download/v#{version}/Marklip-Launcher-#{version}.zip"
+  url "https://github.com/dayflower/marklip-launcher/releases/download/v0.2.0/Marklip-Launcher-0.2.0.zip",
+      verified: "github.com/dayflower/marklip-launcher/"
   name "marklip-launcher"
   desc "Status bar app integrating marklip CLI for clipboard Markdown/HTML conversion"
   homepage "https://github.com/dayflower/marklip-launcher"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
   depends_on arch: :arm64
   depends_on formula: "dayflower/tap/marklip"
 
@@ -21,6 +17,6 @@ cask "marklip-launcher" do
   caveats <<~EOS
     This application is signed with ad-hoc signature.
     You need to run 'xattr -d com.apple.quarantine Marklip\\ Launcher.app'
-    after installation to open it."
+    after installation to open it.
   EOS
 end
