76641a199597ffc55827abe755f1c3cd4dca71e0
ITO Nobuaki <daydream.trippers@gmail.com>
2026-07-27 09:47:17 +0900

    brew-up: update Casks/lidlock.rb for v0.2.0

--- Casks/lidlock.rb
+++ Casks/lidlock.rb
@@ -1,8 +1,8 @@
 cask "lidlock" do
-  version "0.1.4"
-  sha256 "da348f010fa8795fbebb8299157d3bceba62045ae759a06a927cbb3a6df522e1"
+  version "0.2.0"
+  sha256 "492e3682277a695be81fc1b91490f1a7be7fa4bc5d836128a7faa3b03af8f10c"
 
-  url "https://github.com/dayflower/lidlock/releases/download/v0.1.4/LidLock-0.1.4.zip",
+  url "https://github.com/dayflower/lidlock/releases/download/v0.2.0/LidLock-0.2.0.zip",
       verified: "github.com/dayflower/lidlock/"
   name "LidLock"
   desc "Menu-bar app that locks or sleeps a Mac in clamshell mode"
@@ -11,10 +11,4 @@ cask "lidlock" do
   depends_on macos: :ventura
 
   app "LidLock.app"
-
-  caveats <<~EOS
-    This application is signed with an ad-hoc signature.
-    You need to run 'xattr -dr com.apple.quarantine /Applications/LidLock.app'
-    after installation to open it.
-  EOS
 end
