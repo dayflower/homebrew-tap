7b6aeb514814830b5279e0bd7280b4d3ef277536
ITO Nobuaki <daydream.trippers@gmail.com>
2026-09-18 08:51:24 +0900

    brew-up: update Casks/live-transcriber.rb for v0.2.2

--- Casks/live-transcriber.rb
+++ Casks/live-transcriber.rb
@@ -1,8 +1,8 @@
 cask "live-transcriber" do
-  version "0.2.1"
-  sha256 "faf9092e617c3ac9044ccdbe722a8c9eef36aa18cf0c6e72662da313b72b6861"
+  version "0.2.2"
+  sha256 "4004c8f120dec314742cd8f67a4a3fbfef90079b38c94a1e661d5041da1a1862"
 
-  url "https://github.com/dayflower/LiveTranscriber/releases/download/v0.2.1/LiveTranscriber-0.2.1.zip",
+  url "https://github.com/dayflower/LiveTranscriber/releases/download/v0.2.2/LiveTranscriber-0.2.2.zip",
       verified: "github.com/dayflower/live-transcriber/"
   name "LiveTranscriber"
   desc "Real-time on-device speech transcription menu bar app for macOS"
